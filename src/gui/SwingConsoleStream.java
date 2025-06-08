92e8e0de281a5e40d8a6114b34d1f1847963be09
Daniel de Lima Franceschetti <0xfeedcafe@proton.me>
2025-06-08 15:53:44 -0300

    Support UTF-8 in the SwingConsoleStream

--- src/gui/SwingConsoleStream.java
+++ src/gui/SwingConsoleStream.java
@@ -2,13 +2,14 @@ package gui;
 
 import java.awt.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import javax.swing.*;
 import javax.swing.text.*;
 
 class SwingConsoleStream extends OutputStream {
 	private final JTextPane textPane;
 	private final Color color;
-	private final StringBuilder buffer = new StringBuilder();
+	private final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 
 	SwingConsoleStream(JTextPane textPane, Color color) {
 		this.textPane = textPane;
@@ -17,18 +18,19 @@ class SwingConsoleStream extends OutputStream {
 
 	@Override
 	public void write(int b) {
-		buffer.append((char) b);
+		buffer.write(b);
 		if (b == '\n') {
 			SwingUtilities.invokeLater(() -> {
+				String text = buffer.toString(StandardCharsets.UTF_8);
 				StyledDocument doc = textPane.getStyledDocument();
 				Style style = textPane.addStyle("ColorStyle", null);
 				StyleConstants.setForeground(style, color);
 				try {
-					doc.insertString(doc.getLength(), buffer.toString(), style);
+					doc.insertString(doc.getLength(), text, style);
 				} catch (BadLocationException e) {
 					throw new RuntimeException(e);
 				}
-				buffer.setLength(0);
+				buffer.reset();
 			});
 		}
 	}
