2412008bdcf68e6aaaee34d369a017801e909463
Daniel de Lima Franceschetti <0xfeedcafe@proton.me>
2025-06-10 19:22:18 -0300

    Rebuild only changed sources

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-javac -Xlint -d out -sourcepath src src/cli/App.java src/gui/App.java
+mkdir -p out
+
+javac -Xlint -d out -classpath out -sourcepath src src/cli/App.java src/gui/App.java
 
 cp src/icon.png src/*.properties out
 
