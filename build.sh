23f9e084d36f83e366a478a8419887063fd6ef05
Daniel de Lima Franceschetti <0xfeedcafe@proton.me>
2025-06-11 13:21:55 -0300

    Revert "Rebuild only changed sources"
    This reverts commit 2412008bdcf68e6aaaee34d369a017801e909463.
    
    Actually, javac can't rebuild just changed sources, and my attempts to
    intelligently compile just changed files in the shell script were slower
    than just running "javac -sourcepath path/to/src path/to/src/App.java"
    to compile everything again.

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-mkdir -p out
-
-javac -Xlint -d out -classpath out -sourcepath src src/cli/App.java src/gui/App.java
+javac -Xlint -d out -sourcepath src src/cli/App.java src/gui/App.java
 
 cp src/icon.png src/*.properties out
 
